3b5e3e708c86afbd166d59e479d4211010b158ee
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-08 18:43:02 +0300

    Update ازرار2.js

--- plugins/ازرار2.js
+++ plugins/ازرار2.js
@@ -1,13 +1,8 @@
-import pkg from '@whiskeysockets/baileys';
+import pkg from  @whiskeysockets/baileys ;
 const { generateWAMessageFromContent, proto } = pkg
-import { uptime } from '@baileys/core';
 
- var handler = async (m, { conn, usedPrefix }) => {
+var handler = async (m, { conn, usedPrefix }) => {
  
-let users = await conn.fetchBlocklist()
-let uptimeMs = uptime()
-let uptimeText = (uptimeMs / 1000 / 60 / 60).toFixed(2)+'hours' 
-
 let msg = generateWAMessageFromContent(m.chat, {
   viewOnceMessage: {
     message: {
@@ -26,7 +21,7 @@ let msg = generateWAMessageFromContent(m.chat, {
             title: `*━⊱│✫ -『 𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕 』- ✫│⊱━*\n 
         *𝑾𝒆 𝒔𝒕𝒂𝒏𝒅 𝒘𝒊𝒕𝒉 𝒑𝒂𝒍𝒂𝒔𝒕𝒊𝒏𝒆🇵🇸*
  ╭━━━━⊱『 *𝑴𝒊𝒓𝒛𝒂* 』⊱━━━━━╮
-> *👋🏻 مرحباَ:* ${m.pushName}\n> *👥 عدد المستخدمين:* ${users.length}\n> *🟢 وقت النشاط:* ${uptimeText}
+> *👋🏻 مرحباَ:* ${m.pushName}\n> *👥 عدد المستخدمين:* *totalreg*\n> *🟢 وقت النشاط:* %muptime
 ╯━━━━━━━━━━━━━━━━━╰`,
             subtitle: "text",
             hasMediaAttachment: false
@@ -35,7 +30,7 @@ let msg = generateWAMessageFromContent(m.chat, {
             buttons: [
               {
                 "name": "single_select",
-                "buttonParamsJson": "{\"title\":\"القوائم 📑\",\"sections\":[{\"title\":\"𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕\",\"highlight_label\":\"𝑴𝒊ܪﺯ𝒂 𝑩𝒐ت\",\"rows\":[{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"}]}]}"
+                "buttonParamsJson": "{\"title\":\"القوائم 📑\",\"sections\":[{\"title\":\"𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕\",\"highlight_label\":\"𝑴𝒊ܪ𝒛𝒂 𝑩𝒐ت\",\"rows\":[{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"}]}]}"
               },
               {
                 "name": "quick_reply",
