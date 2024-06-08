8c5941aece68f74ba0a2b51f5c9bd519a10efdb6
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-08 18:55:11 +0300

    Update ازرار2.js

--- plugins/ازرار2.js
+++ plugins/ازرار2.js
@@ -4,49 +4,45 @@ const { generateWAMessageFromContent, proto } = pkg
 var handler = async (m, { conn, usedPrefix }) => {
  
 let users = await conn.fetchBlocklist()
-let msg = generateWAMessageFromContent(m.chat, {
-  viewOnceMessage: {
-    message: {
-        "messageContextInfo": {
-          "deviceListMetadata": {},
-          "deviceListMetadataVersion": 2
-        },
-        interactiveMessage: proto.Message.InteractiveMessage.create({
-          body: proto.Message.InteractiveMessage.Body.create({
-            text: ``
-          }),
-          footer: proto.Message.InteractiveMessage.Footer.create({
-            text: "𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕"
-          }),
-          header: proto.Message.InteractiveMessage.Header.create({
-            title: `*━⊱│✫ -『 𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕 』- ✫│⊱━*\n 
+
+let prep = generateWAMessageFromContent(m.chat, {
+  image: await (await require('node-fetch')('https://telegra.ph/file/11d8f4ee53b8dd9fe80c6.jpg')).buffer(),
+  caption: `*━⊱│✫ -『 𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕 』- ✫│⊱━*\n 
         *𝑾𝒆 𝒔𝒕𝒂𝒏𝒅 𝒘𝒊𝒕𝒉 𝒑𝒂𝒍𝒂𝒔𝒕𝒊𝒏𝒆🇵🇸*
  ╭━━━━⊱『 *𝑴𝒊𝒓𝒛𝒂* 』⊱━━━━━╮
-> *👋🏻 مرحباَ:* ${m.pushName}\n> *👥 عدد المستخدمين:* ${users.length}\n> *🟢 وقت النشاط:* %muptime
-╯━━━━━━━━━━━━━━━━━╰`,
-            subtitle: "text",
-            hasMediaAttachment: false
-          }),
-          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-            buttons: [
-              {
-                "name": "single_select",
-                "buttonParamsJson": "{\"title\":\"القوائم 📑\",\"sections\":[{\"title\":\"𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕\",\"highlight_label\":\"𝑴𝒊ܪﺯ𝒂 𝑩𝒐ت\",\"rows\":[{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"}]}]}"
-              },
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\"المطور\",\"id\":\".owner\"}"
-              },
-              {
-                 "name": "cta_url",
-                 "buttonParamsJson": "{\"display_text\":\"قناتنا\",\"url\":\"https://whatsapp.com/channel/0029VagsoU12kNFlfXWaPf0L\",\"merchant_url\":\"https://whatsapp.com/channel/0029VagsoU12kNFlfXWaPf0L\"}"
-              },
-           ],
-          })
-        })
+> *👋🏻 مرحباَ:* ${m.pushName}\n> *👥 عدد المستخدمين:* ${users.length}\n> *🟢 وقت النشاط:* 
+╰━━━━━━━━━━━━━━━━━╰`,
+  footer: "𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕"
+}, {})
+
+let msg = {
+  key: {
+    remoteJid: m.chat,
+    fromMe: true,
+    id: '𝖑00/#{Math.floor(Math.random() * 1000000000000000)}.png'
+  }
+}
+
+let content = prep.message.imageMessage
+content.contextInfo = {
+   forwardingScore: 2,
+   isForwarded: true,
+   externalAdReply: {
+      title: "ᴹᴼᴿᵀᵞ",
+      body: "حمل الان!",
+      thumbnail: await (await require("node-fetch")("https://telegra.ph/file/11d8f4ee53b8dd9fe80c6.jpg")).buffer(),
+      mediaType: 2
+   }
+}
+
+Object.assign(msg, content)
+msg["message"] = {
+  "viewOnceMessage": {
+    "message": {
+        "imageMessage": content
     }
   }
-}, {})
+}
 
 await conn.relayMessage(msg.key.remoteJid, msg.message, { messageId: msg.key.id })
 
