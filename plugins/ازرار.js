08528ec32dcb52fe886f529840794ca8e14bda25
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-08 13:17:17 +0300

    Add files via upload

--- plugins/ازرار.js
+++ plugins/ازرار.js
@@ -1,111 +1,75 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  
-const sections = [
-{
-name: 'single_select',
-buttonParamsJson: {
-title: 'القوائم',
-sections: [
-{
-title: 'Copyright © 2024 dark man.',
-highlight_label: 'بريميوم',
-rows: [
-{
-  
-header: 'بريميوم',
-title: 'قسم اوامر بريميوم',
-//description: '',
-id: '.بريم'
-}
-]
-},
-{
-rows: [
-{
-header: 'القسم الاول',
-title: 'اوامر المجموعات',
-id: 'id'
-},
-{
-header: 'القسم الثاني',
-title: 'اوامر التحويل',
-id: '.id'
-},
-{
-header: 'القسم الثالث',
-title: 'اوامر الادوات',
-id: '.id'
-},
-{
-header: 'القسم الرابع',
-title: 'اوامر الترفيه',
-id: '.id'
-},
-{
-header: 'القسم الخامس',
-title: 'اوامر البحث',
-id: '.id'
-},
-{
-header: 'القسم السادس',
-title: 'اوامر التحميل',
-id: '.id'
-},
-{
-header: 'القسم السابع',
-title: 'اوامر الوجو',
-id: '.id'
-},
-{
-header: 'القسم الثامن',
-title: 'اوامر التأثيرات الصوتية',
-id: '.id'
-},
-{
-header: 'القسم التاسع',
-title: 'اوامر الخاصة بالاعضاء',
-id: '.id'
-},
-{
-header: 'القسم العاشر',
-title: 'اوامر الذكاء الاصطناعي',
-id: '.id'
-}
-]
-},
-  
-]
-}
-},
-{
-name: "quick_reply",
-buttonParamsJson: {
-display_text:"بياناتي",
-id: ".myinfo"
-}
-},
-{
-name: "cta_url",
-buttonParamsJson: {
-display_text:"قناتي",
-url:"https://whatsapp.com/channel/0029VaI4a3JDZ4Lb0AHbdu47",
-merchant_url:"https://whatsapp.com/channel/0029Va14a3JDZ4Lb0AHbdu47"
-}
-}
-];
+import pkg from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent, proto } = pkg
 
-  
-const listMessage = {
-title: `Hello ${m.pushName}!.`,
-text: "معلومات",
-footer: "dark man",
-buttons: sections
-};
-await conn.relayMessage(m.chat, listMessage);
-};
+var handler = async (m, { conn, usedPrefix }) => {
 
-handler.help = ['Buttons']
-handler.tags = ['group']
-handler.command = ['man']
+let msg = generateWAMessageFromContent(m.chat, {
+  viewOnceMessage: {
+    message: {
+        "messageContextInfo": {
+          "deviceListMetadata": {},
+          "deviceListMetadataVersion": 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: "test"
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: "test"
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+            title: "test",
+            subtitle: "test",
+            hasMediaAttachment: false
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [
+              {
+                "name": "single_select",
+                "buttonParamsJson": "{\"title\":\"title\",\"sections\":[{\"title\":\"title\",\"highlight_label\":\"label\",\"rows\":[{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"}]}]}"
+              },
+              {
+                "name": "quick_reply",
+                "buttonParamsJson": "{\"display_text\":\"quick_reply\",\"id\":\"message\"}"
+              },
+              {
+                 "name": "cta_url",
+                 "buttonParamsJson": "{\"display_text\":\"url\",\"url\":\"https://www.google.com\",\"merchant_url\":\"https://www.google.com\"}"
+              },
+              {
+                 "name": "cta_call",
+                 "buttonParamsJson": "{\"display_text\":\"call\",\"id\":\"message\"}"
+              },
+              {
+                 "name": "cta_copy",
+                 "buttonParamsJson": "{\"display_text\":\"copy\",\"id\":\"123456789\",\"copy_code\":\"message\"}"
+              },
+              {
+                 "name": "cta_reminder",
+                 "buttonParamsJson": "{\"display_text\":\"cta_reminder\",\"id\":\"message\"}"
+              },
+              {
+                 "name": "cta_cancel_reminder",
+                 "buttonParamsJson": "{\"display_text\":\"cta_cancel_reminder\",\"id\":\"message\"}"
+              },
+              {
+                 "name": "address_message",
+                 "buttonParamsJson": "{\"display_text\":\"address_message\",\"id\":\"message\"}"
+              },
+              {
+                 "name": "send_location",
+                 "buttonParamsJson": ""
+              }
+           ],
+          })
+        })
+    }
+  }
+}, {})
 
-export default handler;
+await conn.relayMessage(msg.key.remoteJid, msg.message, { messageId: msg.key.id })
+
+}
+handler.command = /^(mboton)$/i
+
+export default handler
