19e10e093e1328c9287c43f981ae0d6ea544c25d
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-09 19:04:19 +0300

    Update ازرار.js

--- plugins/ازرار.js
+++ plugins/ازرار.js
@@ -18,44 +18,37 @@ var handler = async (m, { conn, usedPrefix }) => {
   let msg = generateWAMessageFromContent(m.chat, {
     viewOnceMessage: {
       message: {
-        "messageContextInfo": {
-          "deviceListMetadata": {},
-          "deviceListMetadataVersion": 2
-        },
-        interactiveMessage: proto.Message.InteractiveMessage.create({
-          body: proto.Message.InteractiveMessage.Body.create({
-            image: proto.Message.ImageMessage.create({
-              jpegThumbnail: media
-            })
-          }),
-          footer: proto.Message.InteractiveMessage.Footer.create({
-            text: "𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕"
-          }),
-          header: proto.Message.InteractiveMessage.Header.create({
-            title: `*━⊱│✫ -『 𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕 』- ✫│⊱━*\n 
+        imageMessage: {
+          jpegThumbnail: media
+        }
+      },
+      footer: proto.Message.InteractiveMessage.Footer.create({
+        text: "𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕"
+      }),
+      header: proto.Message.InteractiveMessage.Header.create({
+        title: `*━⊱│✫ -『 𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕 』- ✫│⊱━*\n 
         *𝑾𝒆 𝒔𝒕𝒂𝒏𝒅 𝒘𝒊𝒕𝒉 𝒑𝒂𝒍𝒂𝒔𝒕𝒊𝒏𝒆🇵🇸*
  ╭━━━━⊱『 *𝑴𝒊𝒓𝒛𝒂* 』⊱━━━━━╮
 > *👋🏻 مرحباَ:* *%name*\n> *👥 عدد المستخدمين:* *%totalreg*\n> *🟢 وقت النشاط:* %muptime
 ╯━━━━━━━━━━━━━━━━━╰`,
-            subtitle: "text",
-          }),
-          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-            buttons: [
-              {
-                "name": "single_select",
-                "buttonParamsJson": "{\"title\":\"القوائم 📑\",\"sections\":[{\"title\":\"𝑴𝒊𝒓𝒛𝒂 𝑩𝒐ット\",\"highlight_label\":\"𝑴𝒊𝒓𝒛𝒂 𝑩𝒐ット\",\"rows\":[{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"}]}]}"
-              },
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\"المطور\",\"id\":\".owner\"}"
-              },
-              {
-                 "name": "cta_url",
-                 "buttonParamsJson": "{\"display_text\":\"قناتنا\",\"url\":\"https://whatsapp.com/channel/0029VagsoU12kNFlfXWaPf0L\",\"merchant_url\":\"https://whatsapp.com/channel/0029VagsoU12kNFlfXWaPf0L\"}"
-              },
-           ],
-          })
-        })
+        subtitle: "text",
+      }),
+      nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+        buttons: [
+          {
+            "name": "single_select",
+            "buttonParamsJson": "{\"title\":\"القوائم 📑\",\"sections\":[{\"title\":\"𝑴𝒊𝒓𝒛𝒂 𝑩𝒐ット\",\"highlight_label\":\"𝑴𝒊𝒓𝒛ا 𝑩𝒐ット\",\"rows\":[{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"},{\"header\":\"header\",\"title\":\"title\",\"description\":\"description\",\"id\":\"id\"}]}]}"
+          },
+          {
+            "name": "quick_reply",
+            "buttonParamsJson": "{\"display_text\":\"المطور\",\"id\":\".owner\"}"
+          },
+          {
+             "name": "cta_url",
+             "buttonParamsJson": "{\"display_text\":\"قناتنا\",\"url\":\"https://whatsapp.com/channel/0029VagsoU12kNFlfXWaPf0L\",\"merchant_url\":\"https://whatsapp.com/channel/0029VagsoU12kNFlfXWaPf0L\"}"
+          },
+       ],
+      })
     }
   }, { contextInfo: { mentionedJid: [m.sender] }, quoted: m })
  
