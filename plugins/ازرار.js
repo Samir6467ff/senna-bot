55d431c6d3d8757c7b98c2c47934b86c2f65e082
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-09 18:30:26 +0300

    Update ازرار.js

--- plugins/ازرار.js
+++ plugins/ازرار.js
@@ -54,7 +54,7 @@ let msg = generateWAMessageFromContent(m.chat, {
   mediaMessage: fs.readFileSync('./src/fg_logo.jpg')
 }, { contextInfo: { mentionedJid: [m.sender] }, quoted: m })
  
-await conn.relayMessage(msg.key.remoteJid, msg.message, vn, { messageId: msg.key.id })
+await conn.relayMessage(msg.key.remoteJid, msg.message, mediaMessage, { messageId: msg.key.id })
 
 }
 handler.command = /^(بوت)$/i
