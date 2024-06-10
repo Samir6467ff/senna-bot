ff7b81d2887e11da2d7235d45737bfc3aeaa3951
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-11 00:31:33 +0300

    Update img (1).js

--- plugins/img (1).js
+++ plugins/img (1).js
@@ -41,7 +41,7 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
                 },
             },
         }, { userJid: conn.user.jid, quoted: m })
-        conn.relayMessage(m.chat, msg.message, { messageId: msg.key.participant});
+        conn.relayMessage(m.chat, msg.message, { messageId: msg.key.remoteJid});
         
     } else {
         conn.sendFile(m.chat, 'JoAnimi•Error.jpg', m);      
