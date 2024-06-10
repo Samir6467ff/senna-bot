4c2d102976ed327cc15ff6bab636652d4447a7f0
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-10 17:52:35 +0300

    Update Quran-menu.js

--- plugins/Quran-menu.js
+++ plugins/Quran-menu.js
@@ -129,7 +129,7 @@ let lkr = `
   ❀° ───•••──┄┄──•••───╭
     *♥️القـــــــــرآن الكـــريــــــم♥*
   ╯───•••──┄┄──•••─── °❀`
-conn.sendFile(m.chat, pp, 'perfil.jpg', lkr, m, true, { mentions: [who] })
+conn.sendFile(m.chat, pp, 'perfil.jpg', lkr, m, rcanal, { mentions: [who] })
 m.react(done)
 }
 handler.help = ['قرآن']
