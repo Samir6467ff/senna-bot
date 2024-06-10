892d03afdf8c6c02493b11eba5d4e00ecf6a8a1a
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-11 00:15:31 +0300

    Update img (1).js

--- plugins/img (1).js
+++ plugins/img (1).js
@@ -40,7 +40,7 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
                     interactiveMessage,
                 },
             },
-        }, { userJid: conn.user.jid, quoted: m })
+        }, { contextInfo: { mentionedJid: [m.sender] }, quoted: m })
         conn.relayMessage(m.chat, msg.message, msg.message.viewOnceMessage.message.interactiveMessage.contextInfo = { mentionedJid: [mentionId] });
         
     } else {
