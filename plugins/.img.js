0b668d7ae5aae5b35b1dab89c3068ec4bbad94cd
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-10 12:42:46 +0300

    Update .img.js

--- plugins/.img.js
+++ plugins/.img.js
@@ -7,7 +7,7 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
     const device = await getDevice(m.key.id);
 
     if (device !== 'desktop' || device !== 'web') {      
-        var joanimiimg = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/2784677013ba9efe6cc03.jpg'}}, { upload: conn.waUploadToServer })
+        var joanimiimg = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/11d8f4ee53b8dd9fe80c6.jpg'}}, { upload: conn.waUploadToServer })
         const interactiveMessage = {
             body: { text: `test`.trim() },
             footer: { text: `©JoAnimi for test`.trim() },  
@@ -31,30 +31,6 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
                  "name": "cta_url",
                  "buttonParamsJson": "{\"display_text\":\"url\",\"url\":\"https://www.google.com\",\"merchant_url\":\"\"}"
                         },
-                        {
-                 "name": "cta_call",
-                 "buttonParamsJson": "{\"display_text\":\"call\",\"id\":\"message\"}"
-                        },
-                        {
-                 "name": "cta_copy",
-                 "buttonParamsJson": "{\"display_text\":\"copy\",\"id\":\"123456789\",\"copy_code\":\"message\"}"
-                        },
-                        {
-                 "name": "cta_reminder",
-                 "buttonParamsJson": "{\"display_text\":\"cta_reminder\",\"id\":\"message\"}"
-                        },
-                        {
-                 "name": "cta_cancel_reminder",
-                 "buttonParamsJson": "{\"display_text\":\"cta_cancel_reminder\",\"id\":\"message\"}"
-                        },
-                        {
-                 "name": "address_message",
-                 "buttonParamsJson": "{\"display_text\":\"address_message\",\"id\":\"message\"}"
-                        },
-                        {
-                 "name": "send_location",
-                 "buttonParamsJson": ""
-                        }
   			  		],
                 messageParamsJson: ''
             }
