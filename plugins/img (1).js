5cdfe1993e1ed4be4505bbec78df69ed226d9c18
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-10 23:49:00 +0300

    Update img (1).js

--- plugins/img (1).js
+++ plugins/img (1).js
@@ -7,10 +7,10 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
         var mirzaimg = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/11d8f4ee53b8dd9fe80c6.jpg'}}, { upload: conn.waUploadToServer })
         const mentionId = m.key.participant || m.key.remoteJid;
         const interactiveMessage = {
-            body: { text: `مرحباً! @${m.pushName}`.trim() },
+            body: { text: `مرحباً! ${m.pushName}`.trim() },
             footer: { text: `© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍʀ ʜᴀᴍᴢᴀ`.trim() },  
             header: {
-                title: `السلام عليكم ${mentionId.split('@')[0]}`,
+                title: `السلام عليكم @${mentionId.split('@')[0]}`,
                 subtitle: `test`,
                 hasMediaAttachment: true,
                 imageMessage: mirzaimg.imageMessage,
@@ -42,7 +42,7 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
             },
         }, { userJid: conn.user.jid, quoted: m })
         msg.message.viewOnceMessage.message.interactiveMessage.contextInfo = { mentionedJid: [mentionId] };
-        conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id}, m);
+        conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id}, { userJid: conn.user.jid, quoted: m });
         
     } else {
         conn.sendFile(m.chat, 'JoAnimi•Error.jpg', m);      
