615e7790d93810f0a0a6b31a37d03c1909af6b9c
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-10 18:39:21 +0300

    Update Quran-menu.js

--- plugins/Quran-menu.js
+++ plugins/Quran-menu.js
@@ -19,7 +19,8 @@ let users = participants.map(u => conn.decodeJid(u.id))
             participant: `0@s.whatsapp.net`,
             remoteJid: 'status@broadcast'
         },
-        
+ 
+let finalText = text || q.text       
 let pp = './src/quran.jpg'
 let more = String.fromCharCode(8206)
 let readMore = more.repeat(850) 
@@ -163,6 +164,8 @@ let lkr = `
                     }
                 }
             },
+            { quoted: global.fcontact }
+        )
 conn.sendFile(m.chat, pp, 'perfil.jpg', lkr, m, true, { mentions: [who] }, null, rcanal)
 m.react(done)
 }
