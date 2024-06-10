feb1dc9ad6f16ebd132bf6ea8e37061cd03569eb
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-11 01:41:27 +0300

    Add files via upload

---
+++ plugins/قسم 1.js
@@ -0,0 +1,25 @@
+let handler = async (m, { conn, usedPrefix, command}) => {
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+let pp = './src/quran.jpg'
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(850) 
+let lkr = `
+       *✫━⊱ قسم معلومات البوت ⊱━✫*
+       ╮━━⊱ 「 *معلومات البوت* 」⊱━━╭
+│✫ -  .المحظورين
+│✫ -  .المطور
+│✫ -  .سرعه
+│✫ -  .وقت التشغيل
+│✫ -  .الدعم
+│✫ -  .لسته
+│✫ -  .معلومات
+╯━━━━━━━━━━━━━━━━━╰`
+conn.sendFile(m.chat, pp, 'perfil.jpg', lkr, m, false, { mentions: [who] })
+m.react(done)
+}
+//handler.help = ['قرآن']
+//handler.tags = ['islam']
+handler.command = ['م1', 'quranmenu'] 
+
+export default handler
