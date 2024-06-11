2429742d454978829aadb9906b2bcbad495578d5
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-11 16:33:07 +0300

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -70,7 +70,7 @@ export async function before(m, { conn, text, participants }) {
         sourceUrl: fgcanal
       }
     }
-  }, { quoted: global.fcontact }
+  }
   
     // Forwarded
   global.fwc = {
