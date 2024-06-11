05e8f8f44bdfa8ee7a137150cd23f36f217fc997
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-11 18:28:29 +0300

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -65,7 +65,19 @@ export async function before(m, { conn, text, participants }) {
     }, 
     }
   }
-  
+   global.rcanal = {
+        key: {
+            fromMe: false,
+            participant: `0@s.whatsapp.net`,
+            remoteJid: 'status@broadcast'
+        },
+        message: {
+            contactMessage: {
+                displayName: `${name}`,
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+            }
+        }
+    }
     // Forwarded
   global.fwc = {
     contextInfo: {
