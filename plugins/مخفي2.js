8578a972ed87c7070b2adb6f5ffafbc782928744
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-12 16:07:33 +0300

    Update مخفي2.js

--- plugins/مخفي2.js
+++ plugins/مخفي2.js
@@ -1,29 +1,29 @@
-import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
-import uploadFile from '../lib/uploadFile.js'
+import { generateWAMessageFromContent } from  '@whiskeysockets/baileys' 
+import uploadFile from  '../lib/uploadFile.js' 
 
 let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
     let users = participants.map(u => conn.decodeJid(u.id))
     let q = m.quoted ? m.quoted : m || m.text || m.sender
     let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender
-    let messageType = m.quoted ? q.mtype : 'extendedTextMessage'
-    let messageContent = m.quoted ? c.message[q.mtype] ?? {} : { text: '' || c }
+    let messageType = m.quoted ? q.mtype :  extendedTextMessage 
+    let messageContent = m.quoted ? c.message[q.mtype] ?? {} : { text:    || c }
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; // تعريف المتغير who
 
     if (!(who in global.db.data.users)) throw `✳️ لم يتم العثور على المستخدم في قاعدة البيانات`; // فحص ما إذا كان المستخدم موجودًا في قاعدة البيانات
 
     let { name } = global.db.data.users[who]; // تعريف المتغير name من قاعدة البيانات
-    let pp = 'https://telegra.ph/file/11d8f4ee53b8dd9fe80c6.jpg'
-    m.react('📚')
+    let pp = await conn.profilePictureUrl(who).catch(_=> 'https://telegra.ph/file/24fa902ead26340f3df2c.png') // الحصول على صورة الملف الشخصي للمستخدم
+    m.react( 📚 )
     global.fcontact = {
         key: {
             fromMe: false,
             participant: `0@s.whatsapp.net`,
-            remoteJid: 'status@broadcast'
+            remoteJid:  status@broadcast 
         },
         message: {
             contactMessage: {
                 displayName: `${m.pushName}`,
-                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split( @ )[0]}:${m.sender.split( @ )[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
             }
         }
     }
@@ -40,20 +40,20 @@ let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
 ╯━━━━━━━━━━━━━━━━━╰`
 
     // التحقق مما إذا كان المحتوى المرسل هو صورة أو فيديو
-    if (messageType === 'imageMessage' || messageType === 'videoMessage') {
+    if (messageType ===  imageMessage  || messageType ===  videoMessage ) {
         let media = await q.download()
         let link = await uploadFile(media)
 
         await conn.sendMessage(
             m.chat,
             {
-                [messageType === 'imageMessage' ? 'image' : 'video']: { url: 'https://telegra.ph/file/11d8f4ee53b8dd9fe80c6.jpg' },
+                [messageType ===  imageMessage  ?  image  :  video ]: { url:  'https://telegra.ph/file/11d8f4ee53b8dd9fe80c6.jpg'  },
                 caption: finalText,
                 contextInfo: {
                     mentionedJid: users,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363287489532700@newsletter',
+                        newsletterJid:  120363287489532700@newsletter ,
                         newsletterName: global.packname,
                         serverMessageId: -1
                     }
@@ -70,7 +70,7 @@ let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
                     mentionedJid: users,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363287489532700@newsletter',
+                        newsletterJid:  120363287489532700@newsletter ,
                         newsletterName: global.packname,
                         serverMessageId: -1
                     }
@@ -81,8 +81,8 @@ let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
     }
 }
 
-handler.help = ['مخفي2']
-handler.tags = ['group']
+handler.help = [ مخفي2 ]
+handler.tags = [ group ]
 handler.command = /^(مخفي2|وهمي)$/i
 handler.group = false
 handler.admin = false
