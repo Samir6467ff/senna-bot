33dba12fbb6727fd6cd7aa119e328bc2f02a7520
hamzawy834 <82091562+hamzawy834@users.noreply.github.com>
2024-06-13 02:38:41 +0300

    Update مخفي2.js

--- plugins/مخفي2.js
+++ plugins/مخفي2.js
@@ -1,4 +1,4 @@
-import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
+import { generateWAMessageFromContent } from '@adiwajshing/baileys'
 import uploadFile from '../lib/uploadFile.js'
 
 let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
@@ -12,7 +12,7 @@ let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
     if (!(who in global.db.data.users)) throw `✳️ لم يتم العثور على المستخدم في قاعدة البيانات`; // فحص ما إذا كان المستخدم موجودًا في قاعدة البيانات
 
     let { name } = global.db.data.users[who]; // تعريف المتغير name من قاعدة البيانات
-    let pp = 'https://telegra.ph/file/11d8f4ee53b8dd9fe80c6.jpg'
+    let link = 'https://i.imgur.com/84iBW5F.png'
     m.react('📚')
     global.fcontact = {
         key: {
@@ -42,12 +42,12 @@ let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
     // التحقق مما إذا كان المحتوى المرسل هو صورة أو فيديو
     if (messageType === 'imageMessage' || messageType === 'videoMessage') {
         let media = await q.download()
-        let link = await uploadFile(media)
+        let link = await uploadFile(media.filename)
 
         await conn.sendMessage(
             m.chat,
             {
-                [messageType === 'imageMessage' ? 'image' : 'video']: { url: 'https://telegra.ph/file/11d8f4ee53b8dd9fe80c6.jpg' },
+                [messageType === 'imageMessage' ? 'image' : 'video']: { url: link },
                 caption: finalText,
                 contextInfo: {
                     mentionedJid: users,
@@ -72,3 +72,4 @@ handler.admin = false
 handler.botAdmin = false
 
 export default handler
+```
